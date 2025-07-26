411d7aeb86ad9f2bd926e9a02311390f4515bf09
Stefano Scafiti <stefano.scafiti96@gmail.com>
2025-07-26 17:23:18 +0200

    remove reference to mark3labs

--- server/server_test.go
+++ server/server_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/ostafen/mcp-go/mcp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
